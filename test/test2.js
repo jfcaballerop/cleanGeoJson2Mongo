b0390874c85e2d1e9c870a2494276c8ac7638c51
jfcp <jfcp@inesingenieros.com>
2017-12-05 10:56:31 +0100

    add values and fields created updated and proccessed

---
+++ test/test2.js
@@ -0,0 +1,17 @@
+var cleangjson = require('../lib/cleanGeoJson2Mongo');
+
+
+if (process.argv.length <= 9) {
+    console.log(process.argv);
+    console.log("\n## ERROR ##\n\nUsage: node " + __filename + " path collection host port db user pass authdb");
+    process.exit(-1);
+}
+var path = process.argv[2];
+var collection = process.argv[3];
+var host = process.argv[4];
+var port = process.argv[5];
+var db = process.argv[6];
+var user = process.argv[7];
+var pass = process.argv[8];
+var authdb = process.argv[9];
+console.log('Resultado:: ' + cleangjson.parse2savemongo(path, collection, host, port, db, user, pass, authdb));
